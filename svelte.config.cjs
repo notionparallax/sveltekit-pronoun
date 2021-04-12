534e4ce0b3c9423d82d23dd6b8642e060157a8bf
Ben <ben@notionparallax.co.uk>
2021-04-12 12:03:41 +1000

    first

---
+++ svelte.config.cjs
@@ -0,0 +1,26 @@
+const static = require('@sveltejs/adapter-static');
+// const node = require('@sveltejs/adapter-node');
+const pkg = require('./package.json');
+
+/** @type {import('@sveltejs/kit').Config} */
+module.exports = {
+	kit: {
+		// By default, `npm run build` will create a standard Node app.
+		// You can create optimized builds for different platforms by
+		// specifying a different adapter
+		adapter: {
+			adapt: static,
+			out: 'gh'
+		},
+		// adapter: node(),
+
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte',
+
+		vite: {
+			ssr: {
+				noExternal: Object.keys(pkg.dependencies || {})
+			}
+		}
+	}
+};
